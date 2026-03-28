22ab8e6e37efcc1bf32077d1edcb46ef462410e1
specnerrrrr <specnerrrrr@gmail.com>
2026-03-28 13:43:38 +0800

    Add SmokeBomb class to handle invulnerability effect
    Implement SmokeBomb class with use method for invulnerability effect.

---
+++ src/domain/item/SmokeBomb.java
@@ -0,0 +1,15 @@
+package domain.item;
+
+import domain.combatant.Combatant;
+import domain.effect.InvulnerabilityEffect;
+
+public class SmokeBomb implements Item {
+
+    @Override
+    public void use(Combatant user) {
+
+        user.addEffect(new InvulnerabilityEffect());
+
+        System.out.println("Smoke Bomb used: Enemy damage = 0 for 2 turns");
+    }
+}
