1ab9bf0f6ec8949c13e255f0d1a798500b0c7106
specnerrrrr <specnerrrrr@gmail.com>
2026-03-28 13:51:03 +0800

    Update Player.java

--- src/domain/combatant/Player.java
+++ src/domain/combatant/Player.java
@@ -5,9 +5,22 @@ import domain.action.SpecialSkill;
 public abstract class Player extends Combatant {
 
     protected SpecialSkill specialSkill;
+    protected int skillCooldown = 0;
 
     public Player(String name, int hp, int atk, int def, int spd) {
         super(name, hp, atk, def, spd);
+    }
+
+    public boolean canUseSkill() {
+        return skillCooldown == 0;
+    }
+
+    public void setCooldown(int cd) {
+        this.skillCooldown = cd;
+    }
+
+    public void reduceCooldown() {
+        if (skillCooldown > 0) skillCooldown--;
     }
 
     public SpecialSkill getSpecialSkill() {
