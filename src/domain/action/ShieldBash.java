d0a34af9a34028bfc90ce8a892d1a994fec0f6ed
ZXTG-GIT <zxtg01@gmail.com>
2026-04-03 20:16:26 +0800

    use shield bash before receiving damage

--- src/domain/action/ShieldBash.java
+++ src/domain/action/ShieldBash.java
@@ -16,8 +16,12 @@ public class ShieldBash extends SpecialSkill {
     @Override
     public void activate(Player player, List<Combatant> targets) {
         Combatant target = targets.get(0);
-        player.basicAttack(targets);
-        target.addEffect(new StunEffect());
         System.out.println(GameRenderer.dmg(player.getName() + " uses Shield Bash on " + target.getName() + "!"));
+        
+        int dmg = player.calculateReceivingDamage(player, target);
+        target.takeDamage(dmg);
+        System.out.println(GameRenderer.dmg(player.getName() + " hits " + target.getName() + " for " + dmg + " damage"));
+        
+        target.addEffect(new StunEffect());
     }
 }
